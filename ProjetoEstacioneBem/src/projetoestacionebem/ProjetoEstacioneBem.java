ae1d180c662deb4be736125cbf200fd75dddb119
Emilia <emilia.ferlin@sbsistemas.com.br>
2022-10-14 09:21:52 -0300

    refactor: ajusta logica no main

--- ProjetoEstacioneBem/src/projetoestacionebem/ProjetoEstacioneBem.java
+++ ProjetoEstacioneBem/src/projetoestacionebem/ProjetoEstacioneBem.java
@@ -6,65 +6,54 @@ import java.io.InputStreamReader;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 
-/**Método main que inicializa o projeto e as threads
+/**
+ * Método main que inicializa o projeto e as threads
+ *
  * @author milif
  */
 public class ProjetoEstacioneBem {
 
-    public static void main(String[] args) 
-        throws InterruptedException, IOException{
-            //Declara variaveis utilizadas no main
-            int totalVagas = 12;
-            String namePlaca;
-            String opcaoSelecionada;
-            String colocaGaragem;
-            boolean controleDeLaco = true;
-            
-            //Declara blockingQueue
-            BlockingQueue<Carro> listaCarros = new ArrayBlockingQueue(totalVagas);   
-            
-            //Declara inputs
-            BufferedReader opcaoUsu = new BufferedReader(new InputStreamReader(System.in));
-            BufferedReader placa = new BufferedReader(new InputStreamReader(System.in));
-            BufferedReader insere = new BufferedReader(new InputStreamReader(System.in));
-            
-            //laco de repeticao para programa
-            while(controleDeLaco){
-                //Inicializa
-                System.out.println("Bem vindo ao Estacione Bem!");
-                System.out.println("Deseja continuar S-Sim | N-Não: ");
-                opcaoSelecionada = opcaoUsu.readLine();
-                //Condicao para usuario
-                if (opcaoSelecionada.equalsIgnoreCase("S")) {                                    
-                    System.out.println("Digite sua placa: ");
-                    namePlaca = placa.readLine();
-                    Carro newCar = new Carro(namePlaca);
-                    listaCarros.put(newCar);
-                    
-                    //Condicao se passar de 12 vagas, remove os carros que estao esperando
-                    if(totalVagas > 12){
+    public static void main(String[] args)
+            throws InterruptedException, IOException {
+        //Declara variaveis utilizadas no main
+        String namePlaca;
+        String opcaoSelecionada;
+        boolean controleDeLaco = true;
+
+        //Declara blockingQueue
+        BlockingQueue<Carro> listaCarros = new ArrayBlockingQueue(12);
+
+        //Declara inputs
+        BufferedReader opcaoUsu = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader placa = new BufferedReader(new InputStreamReader(System.in));
+
+        Estacionamento estacione = new Estacionamento(listaCarros);
+        estacione.start();
+
+        Atendente atend = new Atendente(estacione, listaCarros);
+        atend.start();
+
+        //laco de repeticao para programa
+        while (controleDeLaco) {
+            //Inicializa
+            System.out.println("Bem vindo ao Estacione Bem!");
+            System.out.println("Deseja continuar S-Sim | N-Não: ");
+            opcaoSelecionada = opcaoUsu.readLine();
+            //Condicao para usuario
+            if (opcaoSelecionada.equalsIgnoreCase("S")) {
+                System.out.println("Digite sua placa: ");
+                namePlaca = placa.readLine();
+                Carro newCar = new Carro(namePlaca);
+                listaCarros.put(newCar);
+
+                //Condicao se passar de 12 vagas, remove os carros que estao esperando
+                if (listaCarros.size() > 12) {
                     listaCarros.remove();
-                    }
-                    
-                    System.out.println("Deseja colocar na garagem? S-Sim | N-Não: ");
-                    colocaGaragem = insere.readLine();
-                    
-                    //Condicao que inicializa as threads
-                    if(colocaGaragem.equalsIgnoreCase("s")){
-
-                    Estacionamento estacione = new Estacionamento(listaCarros);
-                    estacione.start();
-
-                    Atendente atend = new Atendente(estacione, listaCarros);
-                    atend.start();   
-                    
-                    Thread.interrupted();
-                  
                 }
-
-                } else {
-                    break;
-                }  
-        }        
+                
+            } else {
+                break;
+            }
+        }
     }
 }
